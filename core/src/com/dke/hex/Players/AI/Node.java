28984e4dbe99d45177537b5763d8537114224e34
pdisbeschl <pdisbeschl@gmail.com>
2016-12-01 13:15:14 +0100

    Working on the node

--- core/src/com/dke/hex/Players/AI/Node.java
+++ core/src/com/dke/hex/Players/AI/Node.java
@@ -3,7 +3,8 @@ package com.dke.hex.Players.AI;
 import com.dke.hex.DataStructure.Board;
 import com.dke.hex.DataStructure.Hex;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.LinkedList;
 
 /**
@@ -18,22 +19,34 @@ public class Node {
 
 
 
-    private final Node parent;
+    public Node parent;
     private int numSimulations = 0;
     private Reward reward;
-    private final LinkedList<Node> children = new LinkedList<>();
-    private final /*LinkedList<Move>*/ HashMap<Vector3, Hex> unexploredMoves;
-    private final int player;
-    private final Move moveUsedToGetToNode;
+    public ArrayList<Node> children;
+    public ArrayList<Node> unexploredChildren;
+    public Set<Integer> rVisited;
+    public int player;
+    public double[] pess;
+    public double[] opti;
+    public Move moveUsedToGetToNode;
+    public boolean pruned;
 
-    public Node(Node parent, Move move, Board board) {
-        this.parent = parent;
-        moveUsedToGetToNode = move;
-        unexploredMoves = board.emptySpaces();
+    public Node(Board b){
+        children = new ArrayList<Node>();
+    }
+
+    public Node(Node theParent, Move m, Board theBoard) {
+        children = new ArrayList<Node>();
+        this.parent = theParent;
+        moveUsedToGetToNode = m;
+        Board tempB; //Sim Class --> Copy the board
+        unexploredChildren = theBoard.emptySpaces();
         reward = new Reward(0,0);
         player = null; //Change this
     }
-
+    public double upperConfidenceBound(double c){
+        return reward.getRewardForPlayer(parent.player) / numSimulations + c * Math.sqrt(Math.log(parent.numSimulations+1)/numSimulations);
+    }
     public Node select(){
         Node selectionNode = this;
         double max = Integer.MIN_VALUE;
